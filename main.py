70d6ee7b1fc316097b38ca208524ce0f7eb1eff1
TirolK <tsupiano@gmail.com>
2023-10-24 22:35:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from fastapi import FastAPI
 import requests
+from bs4 import BeautifulSoup
 
 app = FastAPI()
 
 @app.get("/")
 def index():
  
-    url = "https://www.sejuku.net/blog/"
+    url = "https://www.homes.co.jp/trunkroom/b-12663/"
     
     response = requests.get(url)
     response.encoding = response.apparent_encoding
+    bs = BeautifulSoup(response.text, "html.parser")
 
-    return response.text
+    text = bs.find_all('td', {'class': 'btn'})
+
+    return text
