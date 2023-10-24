767f3554fb6957e4f8ba2c1f76a607a55783cbf7
TirolK <tsupiano@gmail.com>
2023-10-25 00:03:53 +0900

    complete

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter, Request
+from fastapi.templating import Jinja2Templates
+
 import requests
 from bs4 import BeautifulSoup
 
-app = FastAPI()
+app = FastAPI(title="Recipe API", openapi_url="/openapi.json")
+api_router = APIRouter()
 
-@app.get("/")
-def index():
- 
-    url = "https://www.homes.co.jp/trunkroom/b-12663/"
-    
-    response = requests.get(url)
-    response.encoding = response.apparent_encoding
-    bs = BeautifulSoup(response.text, "html.parser")
+templates = Jinja2Templates(directory="templates")
 
-    text = bs.find_all('td', {'class': 'btn'})
+@app.get("/", status_code=200)
+def index(request: Request) -> dict:
 
-    return text
+    site_dic = {
+        "ハローストレージ浜尻町": {
+          "url": "https://www.homes.co.jp/trunkroom/b-12661/",
+        },
+        "ハローストレージ貝沢町": {
+          "url": "https://www.homes.co.jp/trunkroom/b-12663/",
+        },
+        "ハローストレージ倉賀野町2": {
+          "url": "https://www.homes.co.jp/trunkroom/b-12667/",
+        },
+        "ハローストレージ東貝沢町": {
+          "url": "https://www.homes.co.jp/trunkroom/b-12668/",
+        },
+    }
+
+    for name, value in site_dic.items():
+        response = requests.get(value["url"])
+        response.encoding = response.apparent_encoding
+        bs = BeautifulSoup(response.text, "html.parser")
+
+        site_dic[name]["price"] = bs.find_all('td', {'class': 'fee'})[-1].get_text()
+        site_dic[name]["size"] = bs.find_all('td', {'class': 'breadth'})[-1].get_text()
+        text = bs.find_all('td', {'class': 'btn'})[-1].get_text()
+        site_dic[name]["result"] = "満室" if "満室のため、お問合せできません" in text else "空室"        
+
+    return templates.TemplateResponse(
+        "index.html", 
+        {"sites": site_dic,"request": request}
+    )
